36c160275fa64888ac15d0746d4b732f678d1229
Filipe Lacerda Oliveira <filipeelacerda@gmail.com>
2020-08-05 22:00:44 -0300

    Web front-end da aplicação NLW proffys
    Front end feito com react e typescript

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../../components/PageHeader';
+function TeacherForm() {
+    return (
+
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Que íncrivel que você quer dar aulas">
+
+            </PageHeader>
+
+        </div>
+    );
+}
+
+export default TeacherForm;
